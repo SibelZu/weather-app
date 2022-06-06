27c594397ec6e9292268bf28d1cb3e61635362c6
SibelZu <sibel-zulfovski@hotmail.com>
2022-06-06 12:56:44 +0200

    Added icon consistant with weather condition from API

--- src/index.js
+++ src/index.js
@@ -37,6 +37,15 @@ function showTemperature(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+  document
+    .querySelector("#icon")
+    .setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+  document
+    .querySelector("#icon")
+    .setAttribute("alt", response.data.weather[0].description);
 }
 
 function handleSubmit(event) {
